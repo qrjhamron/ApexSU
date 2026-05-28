3c2dc5182b927341a756791ca3af410ad565bbe5
qrjhamron <lamabosku1@gmail.com>
2026-05-28 01:46:17 +0000

    ci(website): deploy VitePress site to GitHub Pages

--- website/docs/.vitepress/config.ts
+++ website/docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ import { readdir, writeFile } from 'fs/promises'
 import { resolve } from 'path'
 
 export default defineConfig( {
+    base: '/ApexSU/',
     title: 'ApexSU',
     locales: locales.locales,
     head: [],
